ab981dc58ae977a6d8df797ba1cc6d5d09e0cc8f
An (Martin) Luo <aluo1.developer@gmail.com>
2020-08-13 10:02:20 +0800

    insert, size and getFirst

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,33 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, nextNode = null) {
+    this.data = data;
+    this.next = nextNode;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.listSize = 0;
+  }
+
+  insertFirst(data) {
+    const node = new Node(data);
+    node.next = this.head;
+    this.head = node;
+    this.listSize++;
+  }
+
+  size() {
+    return this.listSize;
+  }
+
+  getFirst() {
+      return this.head;
+  }
+}
 
 module.exports = { Node, LinkedList };
